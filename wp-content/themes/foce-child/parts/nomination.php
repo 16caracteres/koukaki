420e562156f847a3b76427598898a47d11baedc0
16 Caracteres <camille@16caracteres.com>
2024-05-08 15:28:52 +0200

    Ajout nouvelle section Oscars

---
+++ wp-content/themes/foce-child/parts/nomination.php
@@ -0,0 +1,4 @@
+<section id="nomination">
+            <h3>Fleurs d'oranger & chats errants est nominé aux Oscars Short Film Animated de 2022 !</h3>
+            <img class="nomination__logo" src="<?php echo get_stylesheet_directory_uri() . '/assets/logo_oscars.png'; ?>" alt="Logo des Oscars Short Film Animated">
+</section>
