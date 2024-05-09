2ff54e59a78d76ce6d4748f7b2aaa61db08697b0
16 Caracteres <camille@16caracteres.com>
2024-05-09 11:57:28 +0200

    Finalisation Etape 1

--- wp-content/themes/foce-child/js/animations.js
+++ wp-content/themes/foce-child/js/animations.js
@@ -1,6 +1,6 @@
 console.log("coucou")
 
-// Create the observer like the examples above
+// Fade)in des sections au scroll
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
@@ -16,8 +16,23 @@ const observer = new IntersectionObserver(entries => {
   
   // Get multiple elements instead of a single one using "querySelectorAll"
   const sectionAnimation = document.querySelectorAll('.sectionAnimation');
-  
   // Loop over the elements and add each one to the observer
   sectionAnimation.forEach((element) => observer.observe(element));
 
-console.log(sectionAnimation)
+
+
+
+// Animation des titres au scroll
+const observerTitle = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('titreAnimation--scroll');
+        return;
+      }
+      //entry.target.classList.remove('titreAnimation--scroll');
+
+    });
+  });
+  
+  const titreAnimation = document.querySelectorAll('.titreAnimation');
+  titreAnimation.forEach((element) => observerTitle.observe(element));
