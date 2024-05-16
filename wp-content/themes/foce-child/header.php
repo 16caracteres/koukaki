9c3a06e708b634e5a519c27fb9966b1c1dde12ea
16 Caracteres <camille@16caracteres.com>
2024-05-16 11:24:02 +0200

    Finalisation Etape 5

--- wp-content/themes/foce-child/header.php
+++ wp-content/themes/foce-child/header.php
@@ -34,7 +34,16 @@
             
             <div id="burger-navigation">
                 <div class="burger-elements">
+                
+                <!-- floweeeeeers -->
+                    <img class="flower-menu" src="<?php echo get_stylesheet_directory_uri() . '/assets/flowers/orchid.png'; ?> ">
+                    <img class="flower-menu" src="<?php echo get_stylesheet_directory_uri() . '/assets/flowers/Sunflower.png'; ?> ">
+                    <img class="flower-menu" src="<?php echo get_stylesheet_directory_uri() . '/assets/flowers/flower.png'; ?> ">
+                    <img class="flower-menu" src="<?php echo get_stylesheet_directory_uri() . '/assets/flowers/random_flower.png'; ?> ">
+                    
+
                     <img class="logo-burger" src="<?php echo get_stylesheet_directory_uri() . '/assets/logo.png'; ?> " alt="logo Fleurs d'oranger & chats errants">
+                    
                     <ul class="burger-links">
                         <li><a href="#story">Histoire</a></li>
                         <li><a href="#characters">Personnages</a></li>
@@ -42,6 +51,12 @@
                         <li><a href="#studio">Studio Koukaki</a></li>
                     </ul>
                     <p>STUDIO KOUKAKI</p>
+
+                <!-- miaou miaou -->
+                    <img class="cat-menu" src="<?php echo get_stylesheet_directory_uri() . '/assets/potichat/potichat_violet.png'; ?> ">
+                    <img class="cat-menu" src="<?php echo get_stylesheet_directory_uri() . '/assets/potichat/potichat_noir.png'; ?> ">
+                    <img class="cat-menu" src="<?php echo get_stylesheet_directory_uri() . '/assets/potichat/potichat_roux.png'; ?> ">
+                    
                 </div>
             </div>
         </nav>
--- wp-content/themes/foce-child/js/menu.js
+++ wp-content/themes/foce-child/js/menu.js
@@ -4,15 +4,22 @@ const burgerIcons = document.querySelector(".burger-icons");
 const burgerNavigation = document.getElementById("burger-navigation");
 const burgerLinks = document.querySelectorAll(".burger-links li");
 
+console.log(burgerLinks);
 
 burgerIcons.addEventListener("click", () => {
     burgerNavigation.classList.toggle("active");
     burgerIcons.classList.toggle("active");
+    burgerLinks.forEach(link => link.classList.toggle("menu-clic"));
+    document.body.classList.toggle("no-scroll");
+
+
 })
 
 burgerLinks.forEach((link) => {
     link.addEventListener("click", () => {
         burgerNavigation.classList.remove("active");
         burgerIcons.classList.remove("active");
+        burgerLinks.forEach(link => link.classList.remove("menu-clic"));
+        document.body.classList.remove("no-scroll");
     })
 });
